0ad34b9912cbfe679b4dc6cf4c313f2130be7ac3
Niels Sievertsen <nielssievertsen@Nielss-iMac.local>
2014-05-10 20:19:39 +0200

    Initial Commit

---
+++ 100514 OpenCV Hough TransformTests/_00514_OpenCV_Hough_TransformTests.m
@@ -0,0 +1,34 @@
+//
+//  _00514_OpenCV_Hough_TransformTests.m
+//  100514 OpenCV Hough TransformTests
+//
+//  Created by Niels Sievertsen on 10/05/14.
+//  Copyright (c) 2014 Niels Sievertsen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _00514_OpenCV_Hough_TransformTests : XCTestCase
+
+@end
+
+@implementation _00514_OpenCV_Hough_TransformTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
